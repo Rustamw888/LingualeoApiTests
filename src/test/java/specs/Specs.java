72660c62af52992db626655c6e7b9c428e4ff42c
qaMilord <rustamw888@gmail.com>
2022-07-19 01:03:04 +0300

    fix specs

--- src/test/java/specs/Specs.java
+++ src/test/java/specs/Specs.java
@@ -1,13 +1,11 @@
 package specs;
 
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
@@ -26,15 +24,5 @@ public class Specs {
             .log(STATUS)
             .log(BODY)
             .expectStatusCode(200)
-            .build();
-
-    public static ResponseSpecification response403 = new ResponseSpecBuilder()
-            .expectStatusCode(403)
-            .log(ALL)
-            .build();
-
-    public static ResponseSpecification response405 = new ResponseSpecBuilder()
-            .expectStatusCode(405)
-            .log(ALL)
             .build();
 }
